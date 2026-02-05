7fd5f4e1725f7e87fb2041fd98c1c2d7c30175c2
breadddevv <cor.joaocor@gmail.com>
2026-02-05 23:35:08 +0000

    changed theme, why not

--- state/theme.ts
+++ state/theme.ts
@@ -5,11 +5,7 @@ const getInitialTheme = (): "light" | "dark" => {
 		const stored = localStorage.getItem("theme");
 		if (stored === "dark" || stored === "light") return stored;
 	}
-<<<<<<< HEAD
 	return "dark"; // fallback default -- yes BLACK MODE. because light is a crime in society
-=======
-	return "dark"; // fallback default
->>>>>>> df5fe157747e255433e6e19194f21e4b1a0e229f
 };
 
 const __global = globalThis as any;
